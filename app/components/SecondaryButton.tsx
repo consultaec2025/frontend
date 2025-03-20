e30b8fc7ed3c4233d2fe3d43b0b4379dfbdd7d25
wladimov <info@wladimov.com>
2025-03-20 07:35:20 -0500

    Configuration tailwindcss, add main page and componets

---
+++ app/components/SecondaryButton.tsx
@@ -0,0 +1,7 @@
+export default function PrimaryButton({content}) {
+return (
+    <a href="#" className="px-7 py-4 mb-2 bg-[var(--primary-color)] font-bold rounded-3xl text-lg">
+        {content}
+    </a>
+    );
+}
