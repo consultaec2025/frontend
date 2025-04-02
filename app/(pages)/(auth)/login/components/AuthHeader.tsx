c1b79ad55158bff09ec70f33b53e59a53796877f
LeoJP <leojp4781@gmail.com>
2025-04-01 19:52:55 -0500

    feat: Maquetacion de la pagina de login terminada.

---
+++ app/(pages)/(auth)/login/components/AuthHeader.tsx
@@ -0,0 +1,9 @@
+export default function AuthHeader() {
+  return (
+    <div>
+      <h1 className="text-4xl text-white font-bold">
+        Mind<span className="text-[var(--primary-text-color)]">Nest</span>
+      </h1>
+    </div>
+  );
+}
